8731d5df018dce24727413183b47d4c95ecbb527
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2025-02-28 19:45:00 +0900

    450

--- packages/backend/test-federation/test/utils.ts
+++ packages/backend/test-federation/test/utils.ts
@@ -36,7 +36,7 @@ export type Request = <
 
 type Host = 'a.test' | 'b.test';
 
-export async function sleep(ms = 400): Promise<void> {
+export async function sleep(ms = 450): Promise<void> {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
